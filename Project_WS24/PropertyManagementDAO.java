02eba16b851e6572baadf3bb7ecd0421c7555eae
prg-user <dima65000@gmail.com>
2024-10-11 09:40:11 +0200

    NEUE FOLDERN

--- Project_WS24/PropertyManagementDAO.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public interface PropertyManagementDAO 
-{
-    List<Apartment> getApartmentList();
-    Apartment getApartmentById(int id);
-    void saveApartment(Apartment apartment);
-    void deleteApartment(int id);
-}
